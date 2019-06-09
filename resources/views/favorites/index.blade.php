1163052a14c87d343ed5da88e35ff3478b598281
徳井雄介 <tokuicchi@gmail.com>
2019-06-09 10:24:49 +0900

    favorites index edit

--- resources/views/favorites/index.blade.php
+++ resources/views/favorites/index.blade.php
@@ -8,7 +8,7 @@
     @if( $favorites->count() > 0 )
         @foreach($favorites as $favorite )
             <h4>{{$favorite->article_id}}</h4>
-            <h4>{{$articles->title}}</h4>
+            {{--  <h4>{{$articles->title}}</h4>  --}}
             <h4>{{$favorite->user_id}}</h4>
             <hr>
         @endforeach
