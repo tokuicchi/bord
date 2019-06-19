44e2d5e0a8539d350a400861dff4285bd24f3c23
tokuicchi <tokuicchi@gmail.com>
2019-06-19 14:43:31 +0900

    heroku edit

--- config/database.php
+++ config/database.php
@@ -13,9 +13,7 @@ return [
     |
     */
 
-    // 'default' => env('DB_CONNECTION', 'sqlite'),
-    'default' => env('DB_CONNECTION', 'pgsql'),
-    
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
